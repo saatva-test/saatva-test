6ce6ab03477e14a8a3b57662c0a6c62345ef8b56
Monis Rizvi <monisriz@gmail.com>
2021-03-16 17:45:43 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,130 @@
-import logo from './logo.svg';
+import React, {useState, Fragment} from 'react';
+import Rating from '@material-ui/lab/Rating';
 import './App.css';
 
+let products = require('../src/data/products');
+
 function App() {
+  let productTypes = products.mattresses;
+  const [qty, setQty] = useState(0);
+  const [type, setType] = useState('');
+  const [showDisableBtnMsg, setShowDisableBtnMsg] = useState(false);
+  const [classicQty, setClassicQty] = useState(0);
+  const [loomQty, setLoomQty] = useState(0);
+  const [zenQty, setZenQty] = useState(0);
+
+
+  let addCartClickHandler = (e) => {
+    if(e.target.classList.contains('disabledBtnDiv') && !type){
+      console.log('inside addclickhandler if');
+      setShowDisableBtnMsg(true);
+    } else {
+      console.log('inside addclickhandler else');
+      setShowDisableBtnMsg(false);
+      qtyChangeHandler(type);
+    };
+  }
+
+  let qtyChangeHandler = (type) => {
+    console.log('inside addclickhandler if', type);
+    switch (type) {
+      case 'classic':
+        setClassicQty(classicQty + 1);
+        setQty(qty + 1);
+        break;
+      case 'loom':
+        setLoomQty(loomQty + 1);
+        setQty(qty + 1);
+        break;
+      case 'zen':
+        setZenQty(zenQty + 1);
+        setQty(qty + 1);
+        break;
+      default:
+        setQty(qty);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="g-0">
+    <header className="App-header">
+      <svg name="saatvaLogo" data-testid="saatva_logo" role="img" alt="Saatva Logo" description="Saatva Logo" viewBox="0 0 180 38.3" width="180" height="38.3" aria-labelledby="saatvaLogo" fill="#c19678"><title id="saatvaLogo">Saatva Logo</title><path d="M59.8,37.5h-4.9v-4.8c-2.4,3.5-6.4,5.6-10.7,5.6c-8.5,0-15.4-7-15.4-16.2c0-9.3,7-16.3,15.5-16.3c8.8,0,15.5,7.1,15.5,16.3L59.8,37.5z M44.4,10.5c-5.8,0-10.4,5.1-10.4,11.6s4.7,11.5,10.4,11.5c5.7,0,10.4-5.1,10.4-11.5C54.8,15.7,50,10.5,44.4,10.5z"></path><path d="M96.3,37.5h-4.9v-4.8c-2.4,3.5-6.4,5.6-10.7,5.6c-8.5,0-15.4-7-15.4-16.2c0-9.3,7-16.3,15.5-16.3c8.8,0,15.5,7.1,15.5,16.3L96.3,37.5z M80.8,10.5c-5.8,0-10.4,5.1-10.4,11.6s4.7,11.5,10.4,11.5c5.7,0,10.4-5.1,10.4-11.5C91.3,15.7,86.5,10.5,80.8,10.5z"></path><path d="M115,37.5c-7.2,0.8-12.2-3.4-12.2-11.3V0h5v6.6h7v4.9h-7v14.8c0,5.5,3.3,6.8,7.1,6.6V37.5z"></path><path d="M119.2,6.6h5.4l8.5,24.1h0.1l8.5-24.1h5.4l-11.2,30.9h-5.5L119.2,6.6z"></path><path d="M180,37.5h-4.9v-4.8c-2.4,3.5-6.4,5.6-10.7,5.6c-8.5,0-15.4-7-15.4-16.2c0-9.3,7-16.3,15.5-16.3c8.8,0,15.5,7.1,15.5,16.3V37.5z M164.5,10.5c-5.8,0-10.4,5.1-10.4,11.6s4.7,11.5,10.4,11.5c5.7,0,10.4-5.1,10.4-11.5C175,15.7,170.2,10.5,164.5,10.5z"></path><path d="M0,33.2c3.6,3.2,8.2,5,13,4.9c6.8,0,11.2-3.7,11.2-9.3v-0.1c0-4.8-2.8-7.4-10-9.1c-6-1.4-7.6-2.5-7.6-5.1v-0.1c0-2.3,2.2-3.9,5.3-3.9c3.1,0.1,6.1,1.2,8.5,3.2l2.8-4C20,7.2,16,5.8,11.9,5.9c-6.3,0-10.7,3.7-10.7,9V15c0,5.7,3.7,7.7,10.4,9.3c5.5,1.3,7.2,2.4,7.2,5v0.1c0,2.5-2.3,4.1-5.7,4.1c-3.6,0-6.7-1.3-10-4.1L0,33.2z"></path></svg>
+      <div className="cart_icon">
+        <button className="cartIcon">
+          <svg name="cart" className="icon icon--cart" fill="#6b6257" role="img" alt="cart icon" description="Cart" viewBox="0 0 25 25" width="25" height="25" aria-labelledby="cartIcon"><title id="cartIcon">Cart</title><path d="M24.6,5.9c-0.3-0.3-0.7-0.6-1.1-0.6l-18.1-2V3.1C5.2,1.4,3.9,0,2.6,0h-2C0.2,0,0,0.2,0,0.5 C0,0.8,0.2,1,0.5,1h2c0.8,0,1.6,1,1.8,2.1l0.8,8.2c0.1,1.5-0.2,3-0.8,4.4c-0.4,0.6-0.4,1.3,0,1.9c0.5,0.7,1.3,1,2.1,0.9h16.5c0.3,0,0.5-0.2,0.5-0.5c0-0.3-0.2-0.5-0.5-0.5H6.3c-0.4,0.1-0.9-0.1-1.1-0.4c-0.1-0.3-0.1-0.6,0.1-0.9c0.3-0.5,0.5-1.1,0.7-1.7H22c0.9,0,1.6-0.6,1.8-1.4l1.1-6C25,6.7,24.9,6.2,24.6,5.9L24.6,5.9z M22.8,12.9c-0.1,0.3-0.4,0.6-0.7,0.6H6.1c0.1-0.7,0.2-1.5,0.1-2.2L5.5,4.3l17.9,2c0.1,0,0.2,0.1,0.3,0.2c0.1,0.1,0.1,0.2,0.1,0.3L22.8,12.9L22.8,12.9z M19.8,24.2c-1.3,0-2.3-1-2.3-2.3s1-2.3,2.3-2.3c1.3,0,2.3,1,2.3,2.3S21.1,24.2,19.8,24.2z M19.8,20.6c-0.5,0-0.9,0.2-1.1,0.6s-0.2,0.9,0,1.3s0.7,0.6,1.1,0.6c0.7,0,1.3-0.6,1.3-1.3S20.6,20.6,19.8,20.6z M7.8,24.2c-1.3,0-2.3-1-2.3-2.3s1-2.3,2.3-2.3c1.3,0,2.3,1,2.3,2.3S9.1,24.2,7.8,24.2z M7.8,20.6c-0.5,0-0.9,0.2-1.1,0.6s-0.2,0.9,0,1.3s0.7,0.6,1.1,0.6c0.7,0,1.3-0.6,1.3-1.3S8.6,20.6,7.8,20.6z"></path></svg>
+          <span className={"cartIcon__count" + (qty > 0 ? '' : ' hide')} data-testid="cartQtySpan">{qty}</span>
+        </button>
+      </div>
+    </header>
+      <div className="row pt-5">
+        <div className="col-lg-8 px-5 py-4">
+          {type &&
+            <img className="w-100" src={require("./images/"+products.mattresses[type].imageFileName)} alt={products.mattresses[type].name} />
+          }
+          {!type &&
+            <img className="w-100" src={require("./images/hero.jpg")} alt="Saatva Lineup" />
+          }
+          
+        </div>
+        
+        <div className="col-lg-4 px-5 py-4">
+          <div className="row py-4">
+            <span className="body_heading px-0">Choose Your Mattress</span>
+          </div>
+
+          <div className="row">
+            <span className="type-select-label px-0 body-text">SELECT MATTRESS TYPE</span>
+          </div>
+
+          <div className="row pb-4">
+            <div className="btn-group type-select px-0 col-lg-9 col-md-8" role="group">
+              {Object.entries(productTypes).map(([key, value]) => {
+                return (
+                <Fragment key={key}>
+                  <input type="radio" className="btn-check" name="btnradio" id={"btn_"+key} autoComplete="off" onClick={()=>{
+                    setType(key);
+                  }}/>
+                  <label className="btn btn-outline-primary body-text type-btn-text-sm col-3" htmlFor={"btn_"+key} data-testid={"typeBtn_"+key}>{value.name}</label>
+                </Fragment>)
+              })}
+            </div>
+          </div>
+          
+          {type &&
+            <div className="row">
+              <Rating
+                  className="px-0 pb-2 col-lg-9 col-md-8"
+                  name="half-rating-read"
+                  precision={0.10}
+                  readOnly
+                  value={products.mattresses[type].reviewRating}
+                />
+              <div className="pb-2 px-0 type-details col-lg-9 col-md-8">
+                <span className="px-0 body-text"><strong>{products.mattresses[type].name}</strong></span>
+                <span className="px-0 body-text">${products.mattresses[type].price}</span>
+              </div>
+            </div>
+          }
+
+          {showDisableBtnMsg && !type &&
+            <div className="row mb-0">
+              <span className="px-2 mb-0 body-text col-lg-9 col-md-8 alert alert-dark">Select mattress type</span>
+            </div>
+          }
+          
+          <div className="row py-4">
+            {type &&
+              <button type="button" className="btn qtyBtn px-0 body-text col-lg-9 col-md-8" data-testid="qtyBtn" onClick={(e)=>addCartClickHandler(e)}>Add to Cart</button>
+            }
+            {!type &&
+              <div onClick={(e)=>addCartClickHandler(e)} className="disabledBtnDiv col-lg-9 col-md-8 px-0">
+                <button type="button" className="btn qtyBtn body-text w-100 disabled">Add to Cart</button>
+              </div>
+            }
+          </div>
+
+        </div>
+      </div>
+  </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, getByTestId } from '@testing-library/react';
 import App from './App';
+import { get } from 'http';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('saatva_logo');
   expect(linkElement).toBeInTheDocument();
+});
+
+test('qty increases when ATC btn is clicked', () => {
+  render(<App />);
+  fireEvent.click(screen.getByTestId('typeBtn_classic'));
+  fireEvent.click(screen.getByTestId('qtyBtn'));
+  const cartElem = screen.getByTestId('cartQtySpan');
+  expect(cartElem).toContainHTML("1");
 });
